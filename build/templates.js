3255a43821e15a225f349c1ce4b629c473a5920c
John Howard <howardjw@gmail.com>
2014-05-17 18:27:23 -0700

    changes !

--- build/templates.js
+++ build/templates.js
@@ -1,6 +1,6 @@
 this["JST"] = this["JST"] || {};
 
-this["JST"]["hello.html"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<h1>Hello App!</h1>';}return __p};
+this["JST"]["hello.html"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<h1>Hello App!!</h1>';}return __p};
 
 this["JST"]["layout.html"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div class="app">\n\t<div id="menu"></div>\n\t<div id="content"></div>\n</div>';}return __p};
 
